46aeeff5297e8c159286a3cf2043a9cf8b85f615
wsxzwps <wsxzwps43@126.com>
2019-02-17 22:47:25 +0000

    first test version

--- sample.py
+++ sample.py
@@ -123,11 +123,11 @@ else:
 
     # train
     t = SupervisedTrainer(loss=loss, batch_size=32,
-                          checkpoint_every=50,
+                          checkpoint_every=1000,
                           print_every=10, expt_dir=opt.expt_dir)
 
     seq2seq = t.train(seq2seq, train,
-                      num_epochs=6, dev_data=dev,
+                      num_epochs=10, dev_data=dev,
                       optimizer=optimizer,
                       teacher_forcing_ratio=0.5,
                       resume=opt.resume)
