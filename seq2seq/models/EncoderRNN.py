3d678eb3ef6d2b5740b96ad6afca421ad6d73dd5
wsxzwps <wsxzwps43@126.com>
2019-02-17 15:38:45 -0500

    Update EncoderRNN.py

--- seq2seq/models/EncoderRNN.py
+++ seq2seq/models/EncoderRNN.py
@@ -1,5 +1,4 @@
 import torch.nn as nn
-import torch
 
 from .baseRNN import BaseRNN
 
@@ -66,8 +65,6 @@ class EncoderRNN(BaseRNN):
             - **output** (batch, seq_len, hidden_size): variable containing the encoded features of the input sequence
             - **hidden** (num_layers * num_directions, batch, hidden_size): variable containing the features in the hidden state h
         """
-        if torch.cuda.is_available():
-            input_var.cuda()
         embedded = self.embedding(input_var)
         embedded = self.input_dropout(embedded)
         if self.variable_lengths:
