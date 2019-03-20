80aa7b8d7fe0f9b8620e3d9f4341a5b490cf3ad1
ruoyaow <ruoyaow@umich.edu>
2019-03-19 21:01:58 -0400

    use cross entropy now

--- seq2seq/loss/loss.py
+++ seq2seq/loss/loss.py
@@ -156,7 +156,7 @@ class Perplexity(NLLLoss):
 
 class Criterion():
     def __init__(self):
-        self.criterion = nn.NLLLoss()
+        self.criterion = nn.CrossEntropyLoss()
         self.loss = 0
     
     def eval_batch(self, outputs, target, target_lengths):
